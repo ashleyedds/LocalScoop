25115ab86e813936fcdcf9879449f87102322644
JS-Quinn <josephshannonquinn@Gmail.com>
2018-01-25 20:57:40 -0500

    map's in the col

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,75 +1,70 @@
-$(document).ready(function() {
+//Initialize map
+var map, infoWindow;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8
+  });
+  infoWindow = new google.maps.InfoWindow;
 
 
-    //Initialize map
-    var map, infoWindow;
-    function initMap() {
-      map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8
+  //Determine current location, initial latitude/longitude
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      console.log("Initial latitude: " + pos.lat);
+      console.log("Initial longitude: " + pos.lng);
+
+      var initialLat = pos.lat;
+      var initialLng = pos.lng;
+
+
+      //Create marker for current location, make it draggable
+      var marker = new google.maps.Marker({
+        position: pos,
+        map: map,
+        draggable: true,
       });
-      infoWindow = new google.maps.InfoWindow;
 
 
-      //Determine current location, initial latitude/longitude
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-          var pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude
-          };
-
-          console.log("Initial latitude: " + pos.lat);
-          console.log("Initial longitude: " + pos.lng);
-
-          var initialLat = pos.lat;
-          var initialLng = pos.lng;
-
-
-          //Create marker for current location, make it draggable
-          var marker = new google.maps.Marker({
-            position: pos,
-            map: map,
-            draggable: true,
+      //Grab new longitude/latitue from moved marker
+      function markerCoords(markerobject) {
+        google.maps.event.addListener(markerobject, 'dragend', function (evt) {
+          infoWindow.setOptions({
+            content: '<p>Marker dropped: Current Lat: ' + evt.latLng.lat().toFixed(3) + ' Current Lng: ' + evt.latLng.lng().toFixed(3) + '</p>'
           });
 
+          console.log("New latitude: " + evt.latLng.lat());
+          console.log("New longitude: " + evt.latLng.lng());
 
-          //Grab new longitude/latitue from moved marker
-          function markerCoords(markerobject) {
-            google.maps.event.addListener(markerobject, 'dragend', function (evt) {
-              infoWindow.setOptions({
-                content: '<p>Marker dropped: Current Lat: ' + evt.latLng.lat().toFixed(3) + ' Current Lng: ' + evt.latLng.lng().toFixed(3) + '</p>'
-              });
+          var newLat = evt.latLng.lat();
+          var newLng = evt.latLng.lng();
 
-              console.log("New latitude: " + evt.latLng.lat());
-              console.log("New longitude: " + evt.latLng.lng());
-
-              var newLat = evt.latLng.lat();
-              var newLng = evt.latLng.lng();
-
-              infoWindow.open(map, markerobject);
-            });
-
-            google.maps.event.addListener(markerobject, 'drag', function (evt) {
-              console.log("marker is being dragged");
-            });
-          }
-
-          //Call function to capture new location
-          markerCoords(marker);
-
-          infoWindow.open(map);
-          map.setCenter(pos);
-        }, function () {
-          handleLocationError(true, infoWindow, map.getCenter());
+          infoWindow.open(map, markerobject);
         });
 
-
-      } else {
-        // Browser doesn't support Geolocation
-        handleLocationError(false, infoWindow, map.getCenter());
+        google.maps.event.addListener(markerobject, 'drag', function (evt) {
+          console.log("marker is being dragged");
+        });
       }
-    }
+
+      //Call function to capture new location
+      markerCoords(marker);
+
+      infoWindow.open(map);
+      map.setCenter(pos);
+    }, function () {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
 
 
-});
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
