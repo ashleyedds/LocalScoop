fa74870034f918a51bd9936059d123db78262109
Dawn Wages <dawn.wages@gmail.com>
2018-02-01 21:33:07 -0500

    Fixed Logout

--- assets/javascript/firebase2.js
+++ assets/javascript/firebase2.js
@@ -151,3 +151,7 @@ $(document).on("click", "#delete-button", function(){
     
     );
 
+$(document).ready(function(){
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+    });
