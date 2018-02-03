f87fe7e9505659b1bc70ae4dae41c13c27d5a3b8
Ashley Edds <30673908+ashleyedds@users.noreply.github.com>
2018-02-03 16:17:32 -0500

    Delete redirect.js

--- assets/javascript/redirect.js
+++
@@ -1,9 +0,0 @@
-firebase.initializeApp(config);
-
-firebase.auth().onAuthStateChanges(firebaseUser =>{
-    if(firebaseUser){
-        window.location.replace("index.html");
-    } else {
-        console.log("You are not logged in");
-    }
-});
